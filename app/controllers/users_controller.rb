f3c519c02bff8726a6c54a25f7476ea36ef4d15d
sinxor <mouradi.aiman@gmail.com>
2016-09-14 15:02:16 +0100

    add css to user profile + relationships follow/unfollow + helper and views refactor

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
   before_action :authenticate_user!, only: [:edit, :update]
   before_action :check_for_correct_user, only: [:edit, :update]
   before_action :set_user, only: [:show, :edit, :update]
+  before_action :check_for_relationship, only: [:show]
 
   def show
   end
@@ -30,6 +31,12 @@ class UsersController < ApplicationController
     def check_for_correct_user
       unless current_user.id == params[:id].to_i
         redirect_to root_url
+      end
+    end
+
+    def check_for_relationship
+      if user_signed_in? && current_user.following?(@user)
+        @relationship = current_user.active_relationships.find_by(followed_id: @user.id)
       end
     end
 end
