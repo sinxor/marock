c1e409416873937795eb6bd5b8c5245187701d77
sinxor <mouradi.aiman@gmail.com>
2016-09-15 19:00:29 +0100

    CSS for Tags + Sidebar layout & CSS style for sidebar + Featured tags & following tags partials + tags wrapper

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,8 +1,10 @@
 class DashboardsController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_following_tags
 
   def show
     @posts = Post.all.includes(:user)
+    @featured_tags = Tag.all.limit(8) # TODO: Tag.where(featured: true) or something like that.
     if user_signed_in?
       @following_tags = current_user.following_tags
     end
