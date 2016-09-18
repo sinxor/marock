4bfacee21083327ed26e7e7ece3b809fa927f8ff
sinxor <mouradi.aiman@gmail.com>
2016-09-18 18:29:21 +0100

    styled like buttons and refactoring + ajax

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,14 +1,22 @@
+# This controller serves as a parent controller for other likes_controllers. Posts::LikesController for example.
+# Child controller that inherit from this LikesController should implement before_action :set_likeable, which sets @likeable.
 class LikesController < ApplicationController
   before_action :authenticate_user!
 
   def create
     current_user.add_like_to(@likeable)
-    redirect_to :back
+    respond_to do |format|
+      format.html { redirect_to :back }
+      format.js
+    end
   end
 
   def destroy
-    
+
     Like.find(params[:id]).destroy
-    redirect_to :back
+    respond_to do |format|
+      format.html { redirect_to :back }
+      format.js
+    end
   end
 end
