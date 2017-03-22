c59d25c4fbc993d38d3607d065688811a08598f7
sinxor <mouradi.aiman@gmail.com>
2017-03-22 20:55:38 +0000

    region

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -7,8 +7,9 @@ if Rails.env.production?
       config.aws_acl    =  :public_read
 
       config.aws_credentials = {
-        access_key_id:      ENV['S3_KEY'],       # required
-        secret_access_key:  ENV['S3_SECRET']     # required
+        access_key_id:      ENV['S3_ACCESS_KEY'],       # required
+        secret_access_key:  ENV['S3_SECRET_KEY']     # required
+        region:             ENV['AWS_REGION'] # Required
       }
 
       config.aws_attributes = {
