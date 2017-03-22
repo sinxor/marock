497d21d99313ae7edaf259fb556b2a431c896c8b
sinxor <mouradi.aiman@gmail.com>
2017-03-22 20:49:41 +0000

    storage

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -8,7 +8,7 @@ class PictureUploader < CarrierWave::Uploader::Base
 
   # Choose what kind of storage to use for this uploader:
   if Rails.env.production?
-    storage :fog
+    storage :aws
   else
     storage :file
   end
